38f6b1d722ef4a46eb8224830e61914c188d52c0
GerardoRoman <offthegrd3657@gmail.com>
2023-02-01 15:36:17 -0500

    clicked buttons display

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 console.log('linked');
 
 const buttons = document.querySelectorAll('button')
+// selects everything from html that has 'button'
 const screen = document.querySelector('#screen')
+// finds element with id of 'screen'
 console.log(buttons)
 
-const fullScreen = [];
+
+const filledDisplay = [];
+// empty array that is going to store what we clicked
 for (let button of buttons) {
     button.addEventListener('click', function (event) {
+        // waiting for a click to happen before registering anything
         console.log(event.target.innerText)
-        let buttonValue = event.target.innerText
-        fullScreen.push(buttonValue);
-        screen.innerText = fullScreen.join("");
+        // whatever button gets clicked, want to see which button
+        let buttonValue = event.target.innerText;
+        // setting content equal to a variable
+        filledDisplay.push(buttonValue);
+        // adding what we clicked to the array
+        screen.innerText = filledDisplay.join("");
+        // taking everything that's been clicked so far, turning it into a string, and showing it in the display box
     })
 }
+
+let equalButton = document.querySelector(`#equals`);
+equalButton.addEventListener('click', function (event) {
+    // make clicking the equals button evaluate teh expression
+    // in the displayScreen and display the solution
+    const answer = eval(screen.innerText)
+    console.log(answer);
+    screen.innerText = answer;
+})
+
+let clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', function (event) {
+    screen.innerText = ""
+    // add some sort of "remove"
+})
